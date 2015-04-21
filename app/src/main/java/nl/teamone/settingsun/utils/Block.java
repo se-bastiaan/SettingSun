ab06d8ba9df938a63551859e94af1fc0edadbdd9
shemels <shemels@student.ru.nl>
2015-04-21 12:36:26 +0200

    Added Block, direction and position, to keep track of where a block is situated and in which direction it moves.

---
+++ app/src/main/java/nl/teamone/settingsun/utils/Block.java
@@ -0,0 +1,22 @@
+package nl.teamone.settingsun.utils;
+
+/**
+ * Created by shemels on 21-4-2015.
+ */
+public class Block {
+    protected int sizeX;
+    protected int sizeY;
+    private Position pos;
+    public Block(Position p, int x, int y){
+        this.sizeX = x;
+        this.sizeY = y;
+        this.pos.X = p.getX();
+        this.pos.Y = p.getX();
+    }
+    public boolean move(Direction d){
+        int x, y;
+        x = this.pos.X+d.getX();
+        y = this.pos.Y+d.getY();
+        return !(x > 3 || y > 4);
+    }
+}
---
+++ app/src/main/java/nl/teamone/settingsun/utils/Direction.java
@@ -0,0 +1,34 @@
+package nl.teamone.settingsun.utils;
+
+/**
+ * Created by shemels on 21-4-2015.
+ */
+public enum Direction {
+    North(0,-1),
+    East(1,0),
+    South(0,1),
+    West(-1,0);
+
+    private int x, y;
+
+    Direction(int a, int b){
+        this.x=a;
+        this.y=b;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
