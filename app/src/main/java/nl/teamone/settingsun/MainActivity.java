d6f84fca9d30ee33c9be4e918cdc892e7df1bff4
Sébastiaan <se_bastiaan@outlook.com>
2015-04-22 13:43:08 +0200

    Update to AppCompat 22.1.0

--- app/src/main/java/nl/teamone/settingsun/MainActivity.java
+++ app/src/main/java/nl/teamone/settingsun/MainActivity.java
@@ -1,13 +1,13 @@
 package nl.teamone.settingsun;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     TextView mScoreText, mHighScoreText;
 
